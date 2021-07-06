7bcbd2c013bf0bbbf6112feef09b236f9fe18c92
Morteza Jamali <mortezajamali4241@gmail.com>
2021-07-06 19:49:10 +0430

    chore: Home page completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { css } from 'glamor';
 import { SideBar, ISideBarProps, Base, Avatar, Content } from '../components/.';
 import {
   HomeIcon,
@@ -6,19 +7,36 @@ import {
   BadgeCheckIcon,
   AcademicCapIcon,
 } from '@heroicons/react/outline';
+import tile1 from '../public/tile1.svg';
+
+const Styles = ({}) => ({
+  root: css({}),
+  avatar: css({
+    '& img': {
+      filter: 'grayscale(100%)',
+    },
+    '&:before': {
+      content: '""',
+      backgroundImage: `url(${tile1.src})`,
+      position: 'absolute',
+      inset: 0,
+      margin: '-2rem',
+    },
+  }),
+});
 
 const sidebarItems: ISideBarProps['items'] = [
   {
     text: 'home',
     link: '#!',
     icon: HomeIcon,
-    className: 'from-gray-300 via-gray-400 to-gray-500',
+    className: 'from-purple-400 via-pink-500 to-red-500',
   },
   {
     text: 'projects',
     link: '#!',
     icon: FireIcon,
-    className: 'from-purple-400 via-pink-500 to-red-500',
+    className: 'from-gray-300 via-gray-400 to-gray-500',
   },
   {
     text: 'status',
@@ -35,13 +53,15 @@ const sidebarItems: ISideBarProps['items'] = [
 ];
 
 export const Index = () => {
+  const { root, avatar } = Styles({});
+
   return (
-    <Base className="dark bg-gray-500 flex">
-      <div>
+    <Base className="dark bg-gray-500 flex" {...root}>
+      <div className="select-none">
         <SideBar items={sidebarItems} />
       </div>
       <Content>
-        <div className="block w-full h-full relative flex flex-col justify-center items-center">
+        <div className="block w-full h-full relative flex flex-col justify-center items-center select-none">
           <div className="flex">
             <h1 className="text-7xl font-extrabold flex flex-col items-end">
               <span>Welcome to</span>
@@ -49,12 +69,15 @@ export const Index = () => {
             </h1>
             <Avatar
               src="https://avatars.githubusercontent.com/u/42025368?v=4"
-              className="w-60 h-60 ml-10 shadow-lg filter grayscale"
+              className="w-60 h-60 ml-10 shadow-lg"
+              {...avatar}
             />
           </div>
-          <div className="text-3xl font-light">
-            <span className="italic">- Morteza Jamali</span>
-            <p>I'm a software engineer & full-stack web developer</p>
+          <div className="text-3xl mt-2">
+            <span className="italic bg-gray-100 p-2">Morteza Jamali</span>
+            <p className="font-light mt-3">
+              I'm a software engineer & full-stack web developer
+            </p>
           </div>
         </div>
       </Content>
