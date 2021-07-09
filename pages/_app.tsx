ec11cbf0b90321629f0630e1e2e924ce68d7235d
Morteza Jamali <mortezajamali4241@gmail.com>
2021-07-09 23:51:27 +0430

    fix: tailwind configuration fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.css';
 
 export const App = ({ Component, pageProps }: AppProps) => (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,11 @@
 module.exports = {
-  important: true,
   darkMode: 'class',
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US',
-  },
   purge: {
     content: [
       './pages/**/*.{js,ts,jsx,tsx}',
       './components/**/*.{js,ts,jsx,tsx}',
+      './views/**/*.{js,ts,jsx,tsx}',
+      './lib/**/*.{js,ts,jsx,tsx}',
     ],
-  },
-  theme: {
-    extend: {
-      backgroundImage: (theme) => ({
-        check: "url('/icons/check.svg')",
-        landscape: "url('/images/landscape/2.jpg')",
-      }),
-    },
-  },
-  variants: {
-    extend: {
-      backgroundColor: ['checked'],
-      borderColor: ['checked'],
-      inset: ['checked'],
-      zIndex: ['hover', 'active'],
-    },
-  },
-  plugins: [],
-  future: {
-    purgeLayersByDefault: true,
   },
 };
